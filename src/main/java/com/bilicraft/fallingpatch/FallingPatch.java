c252e814221c051eda1af444b5975794951c7ee4
Ghost_chu <2908803755@qq.com>
2021-07-12 21:42:51 +0800

    Use better way to fix

--- src/main/java/com/bilicraft/fallingpatch/FallingPatch.java
+++ src/main/java/com/bilicraft/fallingpatch/FallingPatch.java
@@ -1,21 +1,15 @@
 package com.bilicraft.fallingpatch;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.concurrent.TimeUnit;
-
 public final class FallingPatch extends JavaPlugin implements Listener {
-    private Cache<Player, Long> cache = CacheBuilder.newBuilder()
-            .expireAfterWrite(10, TimeUnit.SECONDS)
-            .build();
+
 
     @Override
     public void onEnable() {
@@ -29,16 +23,9 @@ public final class FallingPatch extends JavaPlugin implements Listener {
     }
     @EventHandler(ignoreCancelled = true)
     public void onJoin(PlayerJoinEvent event){
-        cache.put(event.getPlayer(),System.currentTimeMillis());
-    }
-    @EventHandler(ignoreCancelled = true)
-    public void onJoin(EntityDamageEvent event){
-        if(event.getCause() == EntityDamageEvent.DamageCause.FALL){
-            if(event.getEntity() instanceof Player){
-               if(cache.getIfPresent((Player)event.getEntity()) != null){
-                   event.setCancelled(true);
-               }
-            }
-        }
+       if(event.getPlayer().getLocation().add(0,-1,0).getBlock().getType() == Material.AIR){
+           // 强制落地
+           event.getPlayer().teleport(event.getPlayer().getWorld().getHighestBlockAt(event.getPlayer().getLocation()).getLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+       }
     }
 }
